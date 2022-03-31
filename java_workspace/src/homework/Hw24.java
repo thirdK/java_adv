5837f0a196d9b2de517abb9a70ded09b1790845f
thirdK <dla0625@gmail.com>
2022-03-31 21:41:41 +0900

    a

---
+++ java_workspace/src/homework/Hw24.java
@@ -0,0 +1,121 @@
+package homework;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+/*
+1. 얼음틀 크기를 입력 받는다. N*M 2차원 배열을 생성하고 입력 받은 값에 맞게 0,1로 채운다.
+
+2. 배열의 처음부터 마지막까지 반복문을 돌려서 탐색한다.
+	-> 값이 0이면 dfs 탐색으로 인접한 0을 모두 1로 바꾸고 카운트한다. (방문한 배열을 1로 바꾸어 중복 탐색을 막는다.)
+
+3. dfs를 이용하므로 stack / 재귀 2가지 방법 사용가능하다.
+
+	Stack사용
+	해당 인덱스의 값이 0이면 count를 +1하고 해당 인덱스를 stack에 push하고  pop하여 인접한 칸들의 값을 탐색한다.
+	-> 노드가 아니라 2차원배열이므로 상하좌우 좌표로 이동해야 할거 같다.
+	-> 아니면 4방향으로 link필드를 가지는 노드를 만들어야되는데... 하면 안될거 같다
+
+	인접한 칸들의 값을 확인하여 0이면 stack에 push하고 모든 칸의 탐색이 끝나면  pop하여 다시 
+	탐색 -> 0이면 push -> pop -> 탐색 ....을 반복한다. (pop할때마다 값을 1로 변경하자)
+
+3-1. Stack으로 우선 풀고 재귀로 시도해보자
+
+4. count를 한번만할지 계속카운트하여 끝나면 list에 추가하여 list.size()를 구할지는 구현을 해봐야할거 같다.
+	->해보니까 stack은 카운팅이 가능하고 재귀는 힘들어서 size()를 이용하게됨
+
+ */
+
+public class Hw24 {
+	static int[][] arr;
+	static int cnt, count;	//Stack풀이는 cnt를 사용, 재귀 풀이는 count사용
+	static ArrayList<Integer> result = new ArrayList<Integer>();
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("얼음판 크기를 띄어 쓰기로 구분하여 입력 (n m) : ");
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		sc.nextLine();
+		arr = new int[n][m];
+
+		for (int i = 0; i < n; i++) {	
+			String line = sc.nextLine();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = line.charAt(j) - '0';	//입력받은 값 2차원 배열에 정수로 채우기
+			}
+		}
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (arr[i][j] == 0) { // ***여기서 0을 발견하면 dfs()메소드가 실행됨***
+					dfs(i, j);		//Stack을 이용한 풀이
+					
+					count=0;//이건 의미 없음
+					result.add(dfsR(i,j));	//재귀를 이용한 풀이
+				}
+			}
+		}
+		System.out.println(cnt);	//Stack 결과
+		System.out.println(result.size());	//재귀 결과
+	}
+
+
+	//재귀를 이용한 풀이
+	static int dfsR(int x, int y) {	
+		int[][] udlr = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+		count++;//count는 무의미하다 어차피 List의 길이로 답을 구할거라 아무 값이나 반환해도된다. 반환을 하는게 중요
+		
+		arr[x][y] = 1;	//접근하면 중복탐색을 피하기 위해 1로 변경
+		for (int i = 0; i < 4; i++) { // 연결(인접)가능한 배열은 4방향 이므로 4회반복
+			int newX = x + udlr[i][0];// 좌, 우
+			int newY = y + udlr[i][1];// 상, 하
+			if (newX >= 0 && newX < arr.length && newY >= 0 && newY < arr[0].length) {
+				if(arr[newX][newY]==0) {
+					dfsR(newX, newY);
+				}
+			}
+		}
+		return count;
+	}
+
+//===========================================================================================================
+	//Stack을 이용한 풀이
+	static void dfs(int x, int y) {
+		Stack<XY> stack = new Stack<XY>(); // XY클래스 하단 참고
+
+		int[][] udlr = { { 0, -1 }, { 0, 1 }, { -1, 0 }, { 1, 0 } };
+		// 상하좌우 좌표이동을 x, y 인덱스번호로 표현하기위해 2차원 배열로 만듬
+
+		stack.push(new XY(x, y)); // 우선 메서드에 들어왔다는건 0이라는 뜻이기 때문에 stack에 push하며 cnt를 증가시킨다.
+		cnt++;
+
+		while (!stack.isEmpty()) { // 스택이 공백이 될때까지 반복
+			XY tmp = stack.pop(); // pop()하여 꺼내고
+			arr[tmp.x][tmp.y] = 1; // 꺼낸 객체의 x,y를 인덱스로 사용하여 배열 arr의 요소 값을 바꾼다.
+
+			for (int i = 0; i < 4; i++) { // 연결(인접)가능한 배열은 4방향 이므로 4회반복
+				int newX = tmp.x + udlr[i][0];// 좌, 우
+				int newY = tmp.y + udlr[i][1];// 상, 하
+
+				// 배열의 범위를 벗어나면 index예외 발생하므로 조건문으로 걸러줌
+				if (newX >= 0 && newX < arr.length && newY >= 0 && newY < arr[0].length) {
+					if (arr[newX][newY] == 0)
+						stack.push(new XY(newX, newY));
+				}
+			}
+		}
+
+	}
+}
+
+//stack에 x,y좌표를 저장하기 위해 XY클래스를 만듬
+class XY {
+	int x;
+	int y;
+
+	public XY(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+}
---
+++ jspweb04/WebContent/driver_loading.jsp
@@ -0,0 +1,37 @@
+<%@page import="java.sql.*"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<%
+		//JDBC드라이버 로딩 테스트
+
+	//드라이버 로드
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		out.print("JDBC Driver loading 성공!! by 32임태종<br>");
+	} catch (ClassNotFoundException err) {
+		out.print("JDBC Driver loading 실패!!<br>");
+	}
+
+	//DB 연결
+	String url = "jdbc:mysql://localhost:3306/";
+	String id = "root";
+	String pw = "0000";
+	
+	Connection conn = null;
+	
+	try {
+		conn = DriverManager.getConnection(url, id, pw);
+		out.print("SQL DB 연결 성공!! by 32임태종<br>");
+	} catch (SQLException sqlerr) {
+		out.print("SQL DB 연결 실패!! <br>");
+	}
+	
+	%>
+</body>
+</html>
