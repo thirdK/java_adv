e3aff3316ed85ebf32e915033b6bc9594ac2dad1
thirdK <dla0625@gmail.com>
2022-04-06 18:46:42 +0900

    a

--- java_workspace/src/homework/Hw27.java
+++ java_workspace/src/homework/Hw27.java
@@ -11,20 +11,20 @@ public class Hw27 {
 		System.out.print("정렬할 요소의 수 : ");
 		int len = sc.nextInt();
 		
-		solution1(len);	//방법 1
-		solution2(len);	//방법 2
-		solution3(len); //방법 3
-	}
-//==========================================================================
-	static void solution1(int len) {	//반복문 돌리기
-		System.out.println("첫번째 요소들 입력");
 		int[] arr = new int[len];
-//		int[] result = new int[arr.length];	//result를 사용하면 2개의 배열을 사용하게됨
-		
 		for(int i=0; i<len; i++) {
 			arr[i] = sc.nextInt();
 		}
+		
+		solution1(arr);	//방법 1
+		solution2(arr);	//방법 2
+		solution3(arr); //방법 3
+		//한번에 돌려도 상관 없지만 배열을 매개변수로 보내기 때문에 하나씩 돌리는게 정확함
+	}
+//==========================================================================
+	static void solution1(int[] arr) {	//반복문 돌리기
 		Arrays.sort(arr);					//우선 오름차순 정렬
+//		int[] result = new int[arr.length];	//result를 사용하면 2개의 배열을 사용하게됨
 		
 //		for(int i=0; i<arr.length; i++) {	//result를 사용하는 방법.. 반대로 넣기만하면 됨
 //			result[arr.length-1 - i] = arr[i];
@@ -32,8 +32,8 @@ public class Hw27 {
 		
 		for(int i=0; i<arr.length/2; i++) {	//내부에서 정렬하는 방법
 			int tmp = arr[i];
-			arr[i] = arr[arr.length-1];
-			arr[arr.length-1] = tmp;
+			arr[i] = arr[arr.length-1-i];
+			arr[arr.length-1-i] = tmp;
 		}
 		
 		for(int i : arr) {
@@ -43,17 +43,16 @@ public class Hw27 {
 	}
 //===========================================================================================
 	//Arrays.sort()의 comparator를 람다로 구현해서 역정렬
-	static void solution2(int len) {
-		System.out.println("두번째 요소들 입력");
-		Integer[] arr = new Integer[len];	//Integer배열로 생성
-		for(int i=0; i<len; i++) {
-			arr[i] = sc.nextInt();
+	static void solution2(int[] arr) {
+		Integer[] arr2 = new Integer[arr.length];	//Integer배열로 생성
+		
+		for(int i=0; i<arr.length; i++) {	//옮겨 담기
+			arr2[i] = arr[i];
 		}
+		Arrays.sort(arr2, (i1,i2) -> i2-i1);	
+		//comparator를 람다로 구현하면됨.. 대신 기본자료형은 안되고 래퍼클래스를 사용해야함
 		
-		Arrays.sort(arr, (i1,i2) -> i2-i1);	
-		//comparator를 람다로 구현하면됨 기본자료형은 안되고 래퍼클래스를 사용해야함
-		
-		for(int i : arr) {
+		for(int i : arr2) {
 			System.out.print(i+" ");
 		}
 		System.out.println();
@@ -61,24 +60,10 @@ public class Hw27 {
 	
 //===========================================================================================
 	//stream을 이용하여 역정렬
-	static void solution3(int len) {
-		System.out.println("세번째 요소들 입력");
-		int[] arr = new int[len];
-		for(int i=0; i<len; i++) {
-			arr[i] = sc.nextInt();
-		}
-		int[] result = Arrays.stream(arr)	//스트림 생성
-				.boxed()					//박싱
-				.sorted(Comparator.reverseOrder()) //역정렬
-				.mapToInt(Integer::intValue)//각요소 기본자료형으로 변환(언박싱)
-				.toArray();					//배열로 반환
-		
-		for(int i : result) {
-			System.out.print(i+" ");
-		}
-		
-		System.out.println();
+	static void solution3(int[] arr) {
+		Arrays.stream(arr)	//스트림 생성
+			.boxed()		//박싱
+			.sorted(Comparator.reverseOrder()) //역정렬
+			.forEach(i -> System.out.print(i + " ")); //출력
 	}
-	
-	
 }
